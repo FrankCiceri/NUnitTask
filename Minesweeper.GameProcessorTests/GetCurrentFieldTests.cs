7cfcc6c24d4eeeb60bf96a53a239f0bcf3b68c11
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-28 18:50:20 -0500

    Added 2 test for OpenTest.cs, Added 1 test for GetCurrrentFields.cs

---
+++ Minesweeper.GameProcessorTests/GetCurrentFieldTests.cs
@@ -0,0 +1,60 @@
+﻿using Minesweeper.Core;
+using Minesweeper.Core.Enums;
+using Minesweeper.Core.Models;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Minesweeper.GameProcessorTests
+{
+    [TestFixture]
+    internal class GetCurrentFieldTests
+    {
+       [Test]
+       public void GameProcessorTests_GetCurrentFieldTest_ProperlySetMines() {
+
+
+
+            bool[,] field = { {false, false,true,false },
+                              {false,true, false, false},
+                              { true, false,false,false}, };
+
+            var gameProcessor = new GameProcessor(field);
+
+            var currentField = gameProcessor.GetCurrentField();
+
+
+            for ( var row = 0; row < field.GetLength(0); row++ )
+            {
+                for (var col = 0; col < field.GetLength(1); col++)
+                {
+
+                    try { 
+                        gameProcessor.Open(row, col);
+                    }
+                    catch (Exception e) {
+                        
+                    }
+                    currentField = gameProcessor.GetCurrentField();
+                    var currentPoint = currentField[row, col];
+                    
+                    //if (currentPoint == PointState.Mine) currentPoint=PointState.Close; //here each time we find a mine, we change it to close. Uncomment will cause the test to fail.
+                    Console.Write(currentPoint + "," + field[row, col] + "|");
+
+
+                    var actualStatus = (currentPoint != PointState.Mine);
+                    var expectedStatus = (field[row, col] == true);
+                    if (actualStatus && expectedStatus)
+                    {
+                        throw new Exception($"Mines Should be set properly from the initial field.\n" +
+                            $"{field[row, col]} was expected, got {currentPoint} ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+
+
+    }
+}
